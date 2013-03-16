408c6f90e9250bc8fab825d6208f48cc2fb5be4f
Bill Mill <bill.mill@gmail.com>
2013-03-16 14:41:07 -0400

    adding sequences to lesson 1 manifest

--- lessons_src/1/lesson.py
+++ lessons_src/1/lesson.py
@@ -1,5 +1,8 @@
 {'sounds' : [["a_chord.ogg", "a", "A Chord"],
              ["d_chord.ogg", "d", "D Chord"],
              ["e_chord.ogg", "e", "E Chord"]],
+ 'sequences': [["changes_aead.ogg", ["a", "e", "a", "d"]],
+               ["changes_daed.ogg", ["d", "a", "e", "d"]],
+               ["changes_eaed.ogg", ["e", "a", "e", "d"]]],
  'title' : "Lesson 1 Chords"
 }
