b6e0aee6c88f3dfa87420415d7a852191e778010
Bill Mill <bill.mill@gmail.com>
2013-03-17 10:30:20 -0400

    converting lesson 1 to new manifest format

--- lessons_src/1/lesson.py
+++ lessons_src/1/lesson.py
@@ -1,8 +1,12 @@
-{'sounds' : [["a_chord.ogg", "a", "A Chord"],
-             ["d_chord.ogg", "d", "D Chord"],
-             ["e_chord.ogg", "e", "E Chord"]],
- 'sequences': [["changes_aead.ogg", ["a", "e", "a", "d"]],
-               ["changes_daed.ogg", ["d", "a", "e", "d"]],
-               ["changes_eaed.ogg", ["e", "a", "e", "d"]]],
- 'title' : "Lesson 1 Chords"
+{
+'ssr': [["a_chord.ogg", "a"],
+        ["d_chord.ogg", "d"],
+        ["e_chord.ogg", "e"]],
+'palette': [["a", "A"],
+            ["d", "D"],
+            ["e", "E"]],
+'sequences': [["changes_aead.ogg", ["a", "e", "a", "d"]],
+              ["changes_daed.ogg", ["d", "a", "e", "d"]],
+              ["changes_eaed.ogg", ["e", "a", "e", "d"]]],
+'title': "Lesson 1 Chords"
 }
